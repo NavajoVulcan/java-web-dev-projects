55cc0e42c157a9c7a85df7fa12387590829c9c6a
Rico Rose <r.andereck@gmail.com>
2023-08-16 15:39:30 -0500

    first commit loads of errors

---
+++ datatypes/datatype-examples/src/Circle.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+public class Circle {
+
+
+    public Circle() {
+
+    }
+
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+    }
+}
---
+++ datatypes/datatypes-studio/java/org/launchcode/Area.java
@@ -0,0 +1,24 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+
+public static void main(String[] args) {
+    Area area = new Area();
+    area.calculateArea();
+    }
+    public Area(){
+
+    }
+
+    public void calculatedArea() {
+        Scanner input = new Scanner(System.in);
+        Double myUserRadius = input.nextDouble();
+        Double area = Circle.getArea(myUserRadius);
+
+        System.out.println(area);
+
+        input.close();
+    }
+}
