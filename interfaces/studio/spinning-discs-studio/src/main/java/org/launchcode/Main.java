07ebdd41d09e08eee29333ceb4e31458c94f0e80
Rico Rose <r.andereck@gmail.com>
2023-10-01 19:10:58 -0500

    still getting errors, but finished somewhat

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -2,7 +2,7 @@ package org.launchcode;
 
 public class Main {
     public static void main(String[] args) {
-
+System.out.println("Hello World");
         // TODO: Declare and initialize a CD and a DVD object.
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,64 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
-}
+
+    @Test
+    public void isBalancedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str:"[]"));
+    }
+
+    @Test
+    public void isBalancedWithTwoBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str:"[][]"));
+
+    }
+    @Test
+    public void isBalancedIfMultiBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str:"[[]]"));
+    }
+
+    @Test
+    public void isBalancedIfInterMultiBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str:"[[[]]]"));
+    }
+
+    @Test
+    public void isBalancedIfSuperInterMultiBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str:"[[[[]]]]"));
+    }
+
+    @Test
+    public void isBalancedIfDoubleInterMultiBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str:"[[[[][]]]]"));
+    }
+
+    @Test
+    public void bracketWithLaunchcode() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(str:"[Launchcode]"));
+    }
+
+    @Test
+    public void failsIfNotBalanced() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str:"[]]"));
+    }
+
+    @Test
+    public void failsIfBracketsBackwards(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str:"][" ));
+    }
+
+    @Test
+    public void failsIfOnlyOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str:"["));
+    }
+
+    @Test
+    public void failsIfOnlyClosedBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(str:"]"));
+    }
+
+    @Test
+    public void failsIfNotBalancedLaunchCode() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets((str:"Launch]code["));
+    }
+    }
